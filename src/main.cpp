beb652f7d6320226e59848716e7452be251737c0
Bishal Shrestha <136599301+bigketa10@users.noreply.github.com>
2025-08-25 20:43:13 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 static bool running = true;
 
+bool platform_create_window(int width, int height, char* title);
+
+// Windows Platform
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+// Windows Globals
+
+
+// Platform Implementations
+#endif
+
+
 int main()
 {
+    platform_create_window(1200, 720, "Celeste Motor");
     while (running)
     {
         // Update
